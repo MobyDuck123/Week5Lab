ec1738c7bce8c6086d36a8b0d532bdeba0742f86
MobyDuck123 <152602889+MobyDuck123@users.noreply.github.com>
2024-01-04 22:40:06 +0800

    ThursdayLab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,102 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
 
-    private GameObject focalPoint; 
+    public PowerUpType currentPowerUpType = PowerUpType.None;
+    public GameObject bulletPrefab;
+
+    private bool hasPowerUp = false;
+    private float powerupStrength = 15.0f;
+
+    private GameObject tempBullet;
+    private Coroutine powerUpCountDown;
+    private GameObject FocalPoint;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("FocalPoint");
+        FocalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        playerRb.AddForce(FocalPoint.transform.forward * speed * forwardInput);
+
+        if (currentPowerUpType == PowerUpType.Bullet && Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBullets();
+        }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            currentPowerUpType = other.gameObject.GetComponent<PowerUp>().powerUpType;
+            Destroy(other.gameObject);
+
+            if (powerUpCountDown != null)
+            {
+                StopCoroutine(powerUpCountDown);
+            }
+            powerUpCountDown = StartCoroutine(PowerUpCountDownRoutine());
+        }
+
+
+        if (other.CompareTag("PowerUp2"))
+        {
+            hasPowerUp = true;
+            currentPowerUpType = other.gameObject.GetComponent<PowerUp>().powerUpType;
+            Destroy(other.gameObject);
+
+            if (powerUpCountDown != null)
+            {
+                StopCoroutine(powerUpCountDown);
+            }
+            powerUpCountDown = StartCoroutine(PowerUpCountDownRoutine2());
+        }
+    }
+
+
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        hasPowerUp = false;
+        currentPowerUpType = PowerUpType.None;
+    }
+
+    IEnumerator PowerUpCountDownRoutine2()
+    {
+        yield return new WaitForSeconds(5);
+        hasPowerUp = false;
+        currentPowerUpType = PowerUpType.None;
+    }
+
+    private void ShootBullets()
+    {
+        foreach (var enemy in FindObjectsOfType<Enemy>())
+        {
+            tempBullet = Instantiate(bulletPrefab, transform.position + Vector3.up, Quaternion.identity);
+            tempBullet.GetComponent<Bullet>().Shoot(enemy.transform);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)  
+        { 
+         Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("Player Collided With" + collision.gameObject.name + "with powerup set to" + hasPowerUp);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
+
+    }
+
 }
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PowerUpType
+{
+    None,
+    DoubleBoost,
+    ExtraSpeed,
+    Bullet,
+    DeathRayOfDoom,
+    PowerTackle
+}
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
